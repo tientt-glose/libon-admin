27d48c29b3077e62ff40d87a9998c407a512ec85
Nguyen Tien <tientt@tientts-MacBook-Pro.local>
2021-04-23 18:26:29 +0700

    feat: init module Core and make Api controller

---
+++ Modules/Core/Http/Controllers/ApiController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Modules\Core\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Routing\Controller;
+
+class ApiController extends Controller
+{
+    public function successResponse($data, $message = '')
+    {
+        return $this->apiResponse(1, $data, $message);
+    }
+
+    public function errorResponse($data, $message, $code = 0)
+    {
+        return $this->apiResponse($code, $data, $message);
+    }
+
+    protected function apiResponse($code, $data, $message, $error = [])
+    {
+        return \response()->json([
+            'result'       => $code,
+            'current_time' => time(),
+            'message'      => $message,
+            'data'         => !empty($data) ? $data : new \stdClass(),
+            'error'        => !empty($error) ? $error : new \stdClass()
+        ]);
+    }
+}
